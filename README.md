de400ed03b23a0d4f53863cf7c2ed13319b58aba
Josojmf <joso,jmf@gmail.com>
2024-04-23 13:17:37 +0200

    Primer commit practica final

---
+++ README.md
@@ -0,0 +1,10 @@
+# Jose María Fernández Chat Sincronizado
+## Sincronizado mediante:
+
+
+#RUN:
+````
+npm install
+````
+npm run start
+````
---
+++ index.js
@@ -0,0 +1,48 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+const NTP = require("ntp-time").Client;
+const client = new NTP("a.st1.ntp.br", 123, { timeout: 5000 });
+async function sync() {
+  try {
+    const timeNTP = await client.syncTime();
+    io.emit(
+      "chat message",
+      "NTP Server sync time:" + JSON.stringify(timeNTP.time)
+    );
+    return JSON.stringify(timeNTP.time);
+  } catch (error) {
+    console.error("ERROR-------------------------", error);
+    io.emit("chat message", "");
+  }
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    const ntpRes = sync().then((time) => {
+      return time;
+    });
+
+    var timeAPIURL = "http://worldtimeapi.org/api/timezone/Europe/Madrid";
+    const apiRes = fetch(timeAPIURL)
+      .then((response) => response.json())
+      .then(
+        (data) =>
+          io.emit(
+            "chat message",
+            "API Server sync time:" + new Date(data.datetime)
+          ),
+        io.emit("chat message", "Local time:" + new Date(LocalTime)),
+      );
+  });
+});
+const LocalTime = new Date();
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
